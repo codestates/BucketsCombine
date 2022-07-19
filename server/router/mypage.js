85fcfb1d955c71f3342bf79a0ce81360d62aacb7
mianhe89 <mianhe89@gmail.com>
2022-07-19 23:04:32 +0900

    cards_create branch merge feature/server_test

--- server/router/mypage.js
+++ server/router/mypage.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const createCard = require("../controller/card/createCard");
-//카드 생성 및 태그 생성, 유저카드조인에 생성한 카드 추가
-router.post("/create", createCard);
-// 회원탈퇴, 회원정보수정
-const withdrawalController = require("../controllers/mypages/withdrawal");
-router.post("/withdrawal", withdrawalController);
+// const withdrawalController = require("../controller/mypages/withdrawal");
+
+router.post("/create", createCard); //카드 생성 및 태그 생성, 유저카드조인에 생성한 카드 추가
+// router.post("/withdrawal", withdrawalController); // 회원탈퇴, 회원정보수정
 
 module.exports = router;
--- server/router/users.js
+++ server/router/users.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const signupController = require("../controllers/users/signup");
-const loginController = require("../controllers/users/login");
-const logoutController = require("../controllers/users/logout");
-const authController = require("../controllers/users/auth");
+const signupController = require("../controller/users/signup");
+const loginController = require("../controller/users/login");
+const logoutController = require("../controller/users/logout");
+const authController = require("../controller/users/auth");
 
 router.post("/signup", signupController);
 router.post("/login", loginController);
