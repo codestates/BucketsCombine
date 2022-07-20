11f39afe5374e79cef1a5f79a6848283d9a1d60d
beggin0 <beggin823@gmail.com>
2022-07-20 16:05:23 +0900

    ec2 merge before

--- server/controller/card/cardInfo.js
+++ server/controller/card/cardInfo.js
@@ -5,8 +5,8 @@ module.exports = async (req, res) => {
   const cardinfo = await cards.findAll({});
   const cardHashtagsa = await cardHashtags.findAll({});
   const arr = [];
-  // console.log(cardinfo);
-  console.log("이것은 카드해쉬태그입니다", cardHashtagsa[0].id);
+  console.log(cardinfo[0].dataValues);
+  // console.log("이것은 카드해쉬태그입니다", cardHashtagsa[0].id);
   res.send(cardinfo);
   // cardHashtags의 카드아이디와 지금 카드아이디가 같을떄의 해쉬아이디를 해쉬태그 아이디와 비교
 
@@ -24,6 +24,12 @@ module.exports = async (req, res) => {
   //   };
   //   arr.push(payload);
   // });
+
+  cardinfo.map((el) => {
+    const payload = {
+      el,
+    };
+  });
 };
 
 // 카드의 정보를 홈페이지에 접속하면 줌
