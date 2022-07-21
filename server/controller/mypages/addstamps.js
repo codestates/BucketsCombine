4bebb69bcb65ab6094f54569c65747888f66ac68
mianhe89 <mianhe89@gmail.com>
2022-07-21 15:05:45 +0900

    stamped id add

---
+++ server/controller/mypages/addstamps.js
@@ -0,0 +1,40 @@
+const { userCardJoins } = require("../../models");
+const { stampeds } = require("../../models");
+module.exports = async (req, res) => {
+  const userstampid = await userCardJoins.findAll({
+    where: {
+      cards_id: req.body.cards_id,
+    },
+  });
+
+  if (userstampid[0].stampeds_id === null) {
+    const createStampid = await stampeds.create({}); // 새로만든 스탬프의 아이디
+
+    var result = await userCardJoins.update(
+      {
+        stampeds_id: createStampid.id,
+      },
+      {
+        where: {
+          cards_id: req.body.cards_id,
+        },
+        returning: true, // update 반환값
+        plain: true,
+        // [x,y]첫 번째 요소 x는 항상 영향을 받는 행의 수이고 두 번째 요소 y는 실제 영향을 받는 행
+      }
+    );
+    console.log(result);
+  } else {
+    console.log(result);
+    res.send("이미 스탬프 된 카드입니다.");
+  }
+
+  // console.log(userstampid[0].stampeds_id);
+  res.send(result);
+};
+
+// cards id가 들어왔을때
+// 해당하는userCardjoins.cards_id를 찾고 stamped id 가  Null값이면 스템프아이디 생성 스템프 아이디 넣고
+// id 순차적으로 넣어줌
+
+//
--- server/router/mypage.js
+++ server/router/mypage.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const createCard = require("../controller/card/createCard");
 const mycards = require("../controller/mypages/mycards");
 const usersinfo = require("../controller/mypages/usersinfo");
-const addstamp = require("../controller/mypages/addstamp");
+const addstamp = require("../controller/mypages/addstamps");
 
-router.post("/addstamp", addstamp);
+router.post("/addstamps", addstamp);
 router.post("/create", createCard); //카드 생성 및 태그 생성, 유저카드조인에 생성한 카드 추가
 router.post("/mycards", mycards);
 router.get("/usersinfo", usersinfo);
