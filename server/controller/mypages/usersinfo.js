ca0f71263001611d1297250c84a75d5c042ada6d
mianhe89 <mianhe89@gmail.com>
2022-07-23 16:43:09 +0900

    password 제거

--- server/controller/mypages/usersinfo.js
+++ server/controller/mypages/usersinfo.js
@@ -2,7 +2,20 @@ const { users } = require("../../models");
 
 module.exports = async (req, res) => {
   const usersinfo = await users.findAll({});
-  res.send(usersinfo);
+  const payload = {
+    id: usersinfo.id,
+    username: usersinfo.username,
+    email: usersinfo.email,
+    usertext: usersinfo.usertext,
+    oauthlogin: usersinfo.oauthlogin,
+    userphotourl: usersinfo.userphotourl,
+    gender: usersinfo.gender,
+    age: usersinfo.age,
+    createdAt: usersinfo.createdAt,
+    updatedAt: usersinfo.updatedAt,
+  };
+
+  res.send(payload);
 };
 
 // 비밀번호 제거
