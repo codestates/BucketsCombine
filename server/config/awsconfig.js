211daa61e0c947fa25373f392be84d94e92a409a
beggin0 <beggin823@gmail.com>
2022-08-18 20:44:34 +0900

    delete config

--- server/config/awsconfig.js
+++
@@ -1,6 +0,0 @@
-require("dotenv").config();
-module.exports = {
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_DEFAULT_REGION,
-};
