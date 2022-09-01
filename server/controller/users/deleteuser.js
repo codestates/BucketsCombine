a9028128c15eca182484ced833b5c3c0a376a9f7
mianhe89 <mianhe89@gmail.com>
2022-09-01 15:59:03 +0900

    kakao login.0

--- server/controller/users/deleteuser.js
+++ server/controller/users/deleteuser.js
@@ -1,17 +1,40 @@
 const { users } = require("../../models");
+const { cards } = require("../../models");
+const { cardHashtags } = require("../../models");
 const { userCardJoins } = require("../../models");
 const { isAuthorized } = require("../tokenFunctions");
 module.exports = async (req, res) => {
+  //! test code
+  // const userid = { id: 35 };
+  //! test code
   const userid = await isAuthorized(req);
   if (!isAuthorized(req)) {
     return;
   } else {
-    console.log(userid);
     await userCardJoins.destroy({
       where: {
         users_id: userid.id,
       },
     });
+
+    const userCardId = await cards.findAll({
+      where: {
+        users_id: userid.id,
+      },
+    });
+    const cardIdSet = userCardId.map((el) => el.id);
+    for (el of cardIdSet) {
+      await cardHashtags.destroy({
+        where: { cards_id: el },
+      });
+      await cards.destroy({
+        where: {
+          id: el,
+        },
+      });
+    }
+    console.log(userid);
+
     await users.destroy({
       where: {
         id: userid.id,
@@ -20,3 +43,8 @@ module.exports = async (req, res) => {
     res.clearCookie("jwtAccessToken").send("이용해주셔서 감사했습니다.");
   }
 };
+
+//userid 받아서 유저 아이디에 해당하는 카드 아이디 배열로 정렬한다음
+// 반복문으로 해당 인덱스에 해당하는 카드아이디의 cardHashtag id를 삭제하고
+// 해당 카드를 삭제
+//
--- server/controller/users/kakaologin.js
+++ server/controller/users/kakaologin.js
@@ -1,37 +1,54 @@
-// const axios = require('axios');
-// const { users } = require('../../models');
-// const { generateAccessToken } = require('../tokenFunctions');
+require("dotenv").config();
+const axios = require("axios");
+const { users } = require("../../models");
+const { generateAccessToken } = require("../tokenFunctions");
 
-// module.exports = async (req, res) => {
-//   const code = req.headers['authorization'];
-//   const token = await axios.post(
-//     `https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=${process.env.KAKAO_REST_API_KEY}&redirect_uri=${process.env.KAKAO_REDIRECT_URI}&code=${code}&client_secret=${process.env.KAKAO_SECRET_CODE}`,
-//     {
-//       headers: {
-//         'Content-type': 'application/x-www-form-urlencoded;charset=utf-8',
-//       },
-//     }
-//   );
-//   const kakaoUserInfo = await axios.get('https://kapi.kakao.com/v2/user/me', {
-//     headers: {
-//       Authorization: `Bearer ${token.data.access_token}`,
-//     },
-//   });
-//   const newUserInfo = await users.findOrCreate({
-//     where: { kakaoId: kakaoUserInfo.data.id },
-//     defaults: {
-//       kakaoId: `${kakaoUserInfo.data.id}`,
-//       userName: kakaoUserInfo.data.kakao_account.profile.nickname,
-//     },
-//   });
-//   const payload = { newUserInfo };
-//   const accessToken = generateAccessToken(payload);
-//   return res
-//     .status(200)
-//     .cookie('jwt', accessToken, {
-//       httpOnly: 'true',
-//       sameSite: 'none',
-//       secure: 'true',
-//     })
-//     .json({ token: accessToken, message: '카카오 로그인 성공' });
-// };
+module.exports = async (req, res) => {
+  const code = req.headers["authorization"];
+  //클라이언트에서 카카오로그인인증을 한뒤 서버로 요구하는데 헤더에 인가코드를 보낸다.
+
+  const token = await axios.post(
+    `https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=${process.env.KAKAO_REST_API_KEY}&
+    redirect_uri=${process.env.KAKAO_REDIRECT_URI}&code=${code}&client_secret=${process.env.KAKAO_SECRET_CODE}`,
+    {
+      headers: {
+        "Content-type": "application/x-www-form-urlencoded;charset=utf-8", //
+      },
+    }
+  );
+  //! ****
+  console.log("토큰 내용", token.data);
+  const kakaoUserInfo = await axios.get("https://kapi.kakao.com/v2/user/me", {
+    headers: {
+      Authorization: `Bearer ${token.data.access_token}`,
+    },
+  });
+  // 서버는 토큰에 헤더에 담긴 토큰으로 카카오서버에 유저 정보를 요청한다.
+
+  const newUserInfo = await users.findOrCreate({
+    where: {
+      email: kakaoUserInfo.data.kakao_account.email,
+    },
+    defaults: {
+      email: `${kakaoUserInfo.data.kakao_account.email}`,
+      username: kakaoUserInfo.data.kakao_account.profile.nickname,
+      userphotourl: `${kakaoUserInfo.data.kakao_account.profile.profile_image}`,
+      oauthlogin: "kakao",
+      age: kakaoUserInfo.data.kakao_account.age_range,
+      gender: kakaoUserInfo.data.kakao_account.gender,
+    },
+  });
+  //서버는 카카오 서버에서 받은 내용을 바탕으로 새로운 유저를 탐색 및 DB에 생성
+  const payload = { id: newUserInfo[0].id };
+  const accessToken = generateAccessToken(payload);
+  // 클라이언트에게 "jwtAccessToken"키값의 accessToken 발급. 내용물은 생성된 아이디의 id
+  return res
+    .status(200)
+    .cookie("jwtAccessToken", accessToken, {
+      httpOnly: "true",
+      sameSite: "none",
+      secure: "true",
+    })
+    .json({ token: accessToken, message: "카카오 로그인 성공" });
+  //응답으로 쿠키와 상태200 그리고 메시지를 보낸다.
+};
